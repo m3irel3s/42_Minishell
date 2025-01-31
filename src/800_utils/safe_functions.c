6f398016743c6eac78e99d88cd03bf20460f0bd2
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-01-31 14:10:24 +0000

    remove safe_calloc because calloc is not an authorized function for this project

--- src/800_utils/safe_functions.c
+++ src/800_utils/safe_functions.c
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:58:31 by meferraz          #+#    #+#             */
-/*   Updated: 2025/01/31 13:50:51 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/01/31 14:09:20 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,33 +28,6 @@ void	*ft_safe_malloc(size_t size)
 	void	*ptr;
 
 	ptr = malloc(size);
-	if (!ptr)
-	{
-		write(2, "Error: Memory allocation failed.\n", 33);
-		exit(EXIT_FAILURE);
-	}
-	return (ptr);
-}
-
-/**
- * Allocates memory for an array of elements, each of them initialized to zero.
- *
- * @param count The number of elements to allocate.
- * @param size The size of each element in bytes.
- * @return A pointer to the allocated memory if the allocation was successful,
- *         otherwise the program will exit with an error message.
- *
- * @note This function does not check if the total allocation size (count * size)
- *       is valid (i.e. not zero or overflowed). It is the caller's 
- *       responsibility to ensure the sizes are valid before calling 
- *       this function.
- */
-
-void	*ft_safe_calloc(size_t count, size_t size)
-{
-	void	*ptr;
-
-	ptr = calloc(count, size);
 	if (!ptr)
 	{
 		write(2, "Error: Memory allocation failed.\n", 33);
