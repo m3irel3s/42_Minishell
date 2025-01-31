09f2d24afec7b9c89aee00f8d2ab9f1cae035509
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-01-31 14:02:51 +0000

    implement ft_init_shell function for shell struct

---
+++ src/200_inits/init.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 13:59:06 by meferraz          #+#    #+#             */
+/*   Updated: 2025/01/31 14:01:00 by meferraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+ #include "../inc/minishell.h"
+ 
+/**
+ * Initializes the given shell structure.
+ *
+ * @param shell A pointer to the shell structure to be initialized.
+ * @param envp An array of environment variables passed to the shell.
+ *
+ * This function sets the shell's environment pointer to the provided envp
+ * array and initializes the prompt and input fields to NULL.
+ */
+void	ft_init_shell(t_shell *shell, char **envp)
+{
+	shell->envp = envp;
+	shell->prompt = NULL;
+	shell->input = NULL;
+}
