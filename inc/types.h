5a6284ac5a0b86bf290ced0db9a2967ccda2c478
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-01 13:32:23 +0000

    add token structure

--- inc/types.h
+++ inc/types.h
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:02:45 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/01/31 15:19:41 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/01 13:31:34 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,13 @@ typedef struct s_shell
 	char	*prompt;
 	char	*input;
 }	t_shell;
+
+typedef struct s_token
+{
+	char			*value;
+	int				type;
+	struct s_token	*next;
+}	t_token;
 
 //============================================================================//
 //                                  ENUMS                                     //
