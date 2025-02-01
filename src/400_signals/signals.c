eb678e480efe100dbc7a1bd9cb95609088864bd3
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-01 10:49:38 +0000

    modify singals.c to use signaction over signal to be more robust

--- src/400_signals/signals.c
+++ src/400_signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 09:56:30 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/01 10:28:51 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/01 10:48:30 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,31 @@
 #include <readline/history.h>
 
 static void	ft_handle_sigint(int sig);
+static void	ft_handle_sigquit(int sig);
 
 /**
- * Sets up signal handling for the shell.
+ * @brief Sets up signal handlers for SIGINT and SIGQUIT
  *
- * This function sets up the signal handlers for the shell. It registers
- * a handler for SIGINT (Ctrl+C) that refreshes the prompt on a new line
- * without exiting the shell. It also sets SIGQUIT (Ctrl+\) to be ignored.
+ * This function sets up two signal handlers. The first handles SIGINT and is
+ * responsible for printing a new line and prompting the user again. The second
+ * handles SIGQUIT and is currently empty, but can be used to add extra
+ * functionality.
  */
 void	ft_set_up_signals(void)
 {
-	signal(SIGINT, ft_handle_sigint);
-	signal(SIGQUIT, SIG_IGN);
+	struct sigaction sa_int;
+	struct sigaction sa_quit;
+
+	sa_int.sa_handler = ft_handle_sigint;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = SA_RESTART;
+	if (sigaction(SIGINT, &sa_int, NULL) == -1)
+		perror("Error setting up SIGINT handler");
+	sa_quit.sa_handler = ft_handle_sigquit;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = SA_RESTART;
+	if (sigaction(SIGQUIT, &sa_quit, NULL) == -1)
+		perror("Error setting up SIGQUIT handler");
 }
 
 /**
@@ -48,4 +61,18 @@ static void	ft_handle_sigint(int sig)
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
+}
+
+/**
+ * Handles the SIGQUIT signal (Ctrl+\) during the shell's execution.
+ *
+ * This function is called when the SIGQUIT signal is received. It does
+ * nothing, effectively ignoring the signal. This prevents the shell from
+ * exiting when the user presses Ctrl+\.
+ *
+ * @param sig The signal number (unused in this function).
+ */
+static void	ft_handle_sigquit(int sig)
+{
+	(void)sig;
 }
