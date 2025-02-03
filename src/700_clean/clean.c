8e1c5f23975c94c007c420444e4a300ce466654a
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-03 15:44:33 +0000

    update comment of ft_cleanup

--- src/700_clean/clean.c
+++ src/700_clean/clean.c
@@ -6,17 +6,25 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 14:46:15 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/03 15:42:32 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/03 15:43:55 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
 /**
- * Frees dynamically allocated memory and clears the readline history.
+ * @brief Cleans up and frees all allocated resources within the shell structure.
  *
- * @param shell A pointer to the shell structure containing the memory to be
- *              freed.
+ * This function is responsible for deallocating memory used by the shell's
+ * prompt string, input string, and token linked list. It iterates through the
+ * tokens linked list and frees each token's value and the token itself.
+ * Additionally, it clears the readline history to ensure no residual
+ * allocations are left.
+ *
+ * @param shell A pointer to the shell structure whose resources are to be
+ *              cleaned up.
  */
+
 void	ft_cleanup(t_shell *shell)
 {
 	t_token *current;
