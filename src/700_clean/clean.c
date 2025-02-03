2e3248b789d26ba5857ab96f6d98f6a4731358ab
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-03 15:43:22 +0000

    update ft_clean to free allocated space needed for token_list

--- src/700_clean/clean.c
+++ src/700_clean/clean.c
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 14:46:15 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/01 10:08:48 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/03 15:42:32 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,24 @@
  */
 void	ft_cleanup(t_shell *shell)
 {
+	t_token *current;
+	t_token *next;
+
+	current = shell->tokens;
 	if (shell->prompt)
 		free(shell->prompt);
 	if (shell->input)
 		free(shell->input);
+	if (current)
+	{
+		while(current)
+		{
+			next = current->next;
+			free(current->value);
+			free(current);
+			current = next;
+		}
+		shell->tokens = NULL;
+	}
 	rl_clear_history();
 }
