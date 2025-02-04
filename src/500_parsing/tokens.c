104729da18c4ea4bb6f1d81d8f452fc4089fa2dc
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-04 16:18:19 +0000

    change handling of unmatched quotes

--- src/500_parsing/tokens.c
+++ src/500_parsing/tokens.c
@@ -6,13 +6,12 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 09:45:34 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/04 16:07:24 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/04 16:17:45 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-static void ft_allocate_tokens(t_shell *shell);
 static void ft_process_input(t_shell *shell);
 
 /**
@@ -34,8 +33,10 @@ int	ft_tokenize(t_shell *shell)
 		return (ERROR);
 	ft_process_input(shell);
 	if (shell->parser->quote_state != NO_QUOTE)
-		return (SUCCESS);
-	ft_allocate_tokens(shell);
+	{
+		ft_putstr_fd("Error: Unmatched quotes.\n", STDERR_FILENO);
+		return (ERROR);
+	}
 	shell->parser->index = 0;
 	shell->parser->token_count = 0;
 	shell->parser->state = STATE_GENERAL;
@@ -44,27 +45,7 @@ int	ft_tokenize(t_shell *shell)
 	if (ft_process_and_tokenize(shell) == ERROR)
 		return (ERROR);
 	return (SUCCESS);
-	return (SUCCESS);
 }
-
-/**
- * @brief Allocates memory for the tokens list based on the parser's token count.
- *
- * If the parser's token count is 0 or negative, the function does nothing.
- *
- * @param shell A pointer to the shell structure whose tokens list is to be
- *              allocated.
- */
-static void ft_allocate_tokens(t_shell *shell)
-{
-	int token_count;
-
-	token_count = shell->parser->token_count;
-	if (token_count <= 0)
-		return ;
-	shell->tokens = ft_safe_malloc(token_count * sizeof(t_token));
-}
-
 
 /**
  * @brief Processes the input string and updates the parser's state.
@@ -102,4 +83,58 @@ static void	ft_process_input(t_shell *shell)
 			return ;
 		i++;
 	}
+}
+
+static t_status ft_process_and_tokenize(t_shell *shell)
+{
+	const char *input = shell->input;
+    size_t start = 0;
+    size_t i = 0;
+    t_token *token;
+
+    while (i <= shell->parser->index)
+    {
+        if (shell->parser->state == STATE_GENERAL)
+        {
+            if (ft_is_space(input[i]))
+                start = ++i;
+            else
+            {
+                shell->parser->state = ft_is_operator(input[i]) ? STATE_IN_OPERATOR : STATE_IN_WORD;
+                shell->parser->token_count++;
+                start = i;
+            }
+        }
+        else if (shell->parser->quote_state != NO_QUOTE || shell->parser->state == STATE_IN_WORD)
+        {
+            if ((shell->parser->quote_state == NO_QUOTE && ft_is_space(input[i])) ||
+                (shell->parser->quote_state != NO_QUOTE && input[i] == '\'' && shell->parser->quote_state == SINGLE_QUOTE) ||
+                (shell->parser->quote_state != NO_QUOTE && input[i] == '"' && shell->parser->quote_state == DOUBLE_QUOTE))
+            {
+                token = ft_create_token(strndup(input + start, i - start), ft_determine_token_type(strndup(input + start, i - start)));
+                ft_add_token_to_list(shell, token);
+                shell->parser->state = STATE_GENERAL;
+                shell->parser->quote_state = NO_QUOTE;
+                start = i + 1;
+            }
+        }
+        else if (shell->parser->state == STATE_IN_OPERATOR)
+        {
+            if (!ft_is_operator(input[i]) || (i > 0 && input[i] != input[i - 1]))
+            {
+                token = ft_create_token(strndup(input + start, i - start), ft_determine_token_type(strndup(input + start, i - start)));
+                ft_add_token_to_list(shell, token);
+                shell->parser->state = STATE_GENERAL;
+                start = i;
+            }
+        }
+        i++;
+    }
+    // Handle any remaining text
+    if (i > start)
+    {
+        token = ft_create_token(strndup(input + start, i - start), ft_determine_token_type(strndup(input + start, i - start)));
+        ft_add_token_to_list(shell, token);
+    }
+    return (SUCCESS);
 }
