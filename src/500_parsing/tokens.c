f2f8973aa636ad1474e57db523648681bb30d15e
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-04 16:10:57 +0000

    implement ft_process_input and remove ft_allocate_tokens function

--- src/500_parsing/tokens.c
+++ src/500_parsing/tokens.c
@@ -6,11 +6,13 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 09:45:34 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/04 12:34:26 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/04 15:45:17 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+static void ft_allocate_tokens(t_shell *shell);
 
 /**
  * @brief Tokenizes the input string and populates the shell structure's tokens list.
@@ -41,12 +43,57 @@ int	ft_tokenize(t_shell *shell)
 	return (SUCCESS);
 }
 
+/**
+ * @brief Allocates memory for the tokens list based on the parser's token count.
+ *
+ * If the parser's token count is 0 or negative, the function does nothing.
+ *
+ * @param shell A pointer to the shell structure whose tokens list is to be
+ *              allocated.
+ */
 static void ft_allocate_tokens(t_shell *shell)
-|{
+{
 	int token_count;
 
 	token_count = shell->parser->token_count;
 	if (token_count <= 0)
 		return ;
 	shell->tokens = ft_safe_malloc(token_count * sizeof(t_token));
+}
+
+/**
+ * @brief Tokenizes the shell's input string and populates the tokens list.
+ *
+ * This function loops through the input string and delegates the handling of
+ * each character to one of the state handler functions. The state handler
+ * functions process the character and update the parser's state accordingly.
+ *
+ * If the parser is in a quote state and the end of the input string is reached,
+ * the function stops processing and returns. Otherwise, the function continues
+ * until the end of the input string is reached.
+ *
+ * @param shell A pointer to the shell structure containing the input string
+ *              to be tokenized and the tokens list to be populated.
+ */
+static void	ft_process_and_tokenize(t_shell *shell)
+{
+	const char	*input;
+	size_t		i;
+
+	input = shell->input;
+	i = shell->parser->index;
+	while (input[i])
+	{
+		if (shell->parser->state == STATE_GENERAL)
+			ft_handle_general_state(shell->parser, input);
+		else if (shell->parser->quote_state != NO_QUOTE)
+			ft_handle_quote_state(shell->parser, input);
+		else if (shell->parser->state == STATE_IN_WORD)
+			ft_handle_word_state(shell->parser, input);
+		else if (shell->parser->state == STATE_IN_OPERATOR)
+			ft_handle_operator_state(shell->parser, input);
+		if (input[i] == '\0' && shell->parser->quote_state != NO_QUOTE)
+			return;
+		i++;
+	}
 }
