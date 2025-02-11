046869578b610a0395e912029ee02baa844806a8
Melanie Ferraz Reis <meferraz@c1r2s6.42porto.com>
2025-02-11 12:24:14 +0000

    update header

--- inc/prototypes.h
+++ inc/prototypes.h
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:02:45 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/02/11 11:26:13 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/11 12:23:53 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,18 @@ void			ft_handle_sigquit(int sig);
 /* 500_parsing*/
 /*parsing.c*/
 int				ft_parse_input(t_shell *shell);
+
 /*tokens.c*/
-int ft_tokenize(t_shell *shell);
+int				ft_tokenize(t_shell *shell);
+
 /*tokens_utils.c*/
 void			ft_add_token_to_list(t_shell *shell, t_token *new_token);
 t_token			*ft_create_token(char *value, t_token_type type);
 t_token_type	ft_determine_token_type(char *value, size_t len);
-void			ft_set_command_type(t_token *token);
-size_t			ft_get_operator_length(const char *input);
-int				ft_create_and_add_token(t_shell *shell, size_t start, size_t end, t_token_type type, int quoted);
+
+/*tokens_utils_2.c*/
+int				ft_create_and_add_token(t_shell *shell, size_t start,
+					size_t end, int quoted);
 
 /* 700_clean*/
 /*clean.c*/
@@ -54,7 +57,8 @@ int				ft_is_operator(char c);
 int				ft_is_space(char c);
 int				ft_is_quote(char c);
 int				ft_is_command(char *value, size_t len);
-/*safe_funcitons.c */
+
+/*safe_functions.c */
 void			*ft_safe_malloc(size_t size);
 char			*ft_safe_readline(t_shell *shell);
 
