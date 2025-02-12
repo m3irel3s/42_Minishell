53f3828b5db2621d1835620ebc990096efdb54c0
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-12 12:44:11 +0000

    create backup to ensure redirection changes are only temporary

--- src/600_exec/exec.c
+++ src/600_exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:18:55 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/02/12 10:16:48 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/12 12:43:13 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,29 @@
 
 void	ft_execute_input(t_shell *shell)
 {
+	int	saved_stdin;
+	int	saved_stdout;
+
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	if (saved_stdin == -1 || saved_stdout == -1)
+	{
+		ft_putstr_fd("minishell: error duplicating file descriptors\n",
+			STDERR_FILENO);
+		return ;
+	}
 	ft_handle_redirections(shell);
-	if (ft_strncmp(shell->tokens->value, "echo", 5) == SUCCESS)
+	if (ft_strncmp(shell->tokens->value, "echo", 5) == 0)
 		ft_echo(shell);
-	if (ft_strncmp(shell->tokens->value, "cd", 3) == SUCCESS)
+	else if (ft_strncmp(shell->tokens->value, "cd", 3) == 0)
 		ft_cd(shell);
-	if (ft_strncmp(shell->tokens->value, "env", 3) == SUCCESS)
+	else if (ft_strncmp(shell->tokens->value, "env", 3) == 0)
 		ft_env(shell);
-	return ;
+	if (dup2(saved_stdin, STDIN_FILENO) == -1 ||
+		dup2(saved_stdout, STDOUT_FILENO) == -1)
+		ft_putstr_fd("minishell: error restoring file descriptors\n",
+			STDERR_FILENO);
+	close(saved_stdin);
+	close(saved_stdout);
 }
+
