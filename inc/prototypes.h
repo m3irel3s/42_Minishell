9b72ae5dec7774cfd2e7f72d45f023d9f82ad415
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-12 10:13:09 +0000

    implement ft_create_redirection_list and update header file

--- inc/prototypes.h
+++ inc/prototypes.h
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   prototypes.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
+/*   By: meferraz      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:02:45 by jmeirele          #+#    #+#             */
 /*   Updated: 2025/02/11 16:16:01 by meferraz         ###   ########.fr       */
@@ -13,79 +13,122 @@
 #ifndef PROTOTYPES_H
 # define PROTOTYPES_H
 
+//============================================================================//
+//                                INTRODUCTION                                //
+//============================================================================//
 /* 000_intro.c */
-void			ft_display_startup_banner(void);
-int				ft_authors(void);
+void		ft_display_startup_banner(void);
+int		ft_authors(void);
 
-/* 200_inits*/
-/*init.c*/
-void			ft_init_shell(t_shell *shell, char **envp);
+//============================================================================//
+//                             INITIALIZATION                                 //
+//============================================================================//
+/* 200_inits/init.c */
+void		ft_init_shell(t_shell *shell, char **envp);
 
-/* 300_prompt*/
-/*prompt.c*/
-char			*ft_set_prompt(void);
-char			*ft_get_current_directory(void);
+//============================================================================//
+//                               PROMPT HANDLING                              //
+//============================================================================//
+/* 300_prompt/prompt.c */
+char		*ft_set_prompt(void);
+char		*ft_get_current_directory(void);
 
-/* 400_signals*/
-/*signals.c*/
-void			ft_set_up_signals(void);
-void			ft_handle_sigint(int sig);
-void			ft_handle_sigquit(int sig);
+//============================================================================//
+//                              SIGNAL HANDLING                               //
+//============================================================================//
+/* 400_signals/signals.c */
+void		ft_set_up_signals(void);
+void		ft_handle_sigint(int sig);
+void		ft_handle_sigquit(int sig);
 
-/* 500_parsing*/
+//============================================================================//
+//                                  PARSING                                   //
+//============================================================================//
+/* 500_parsing/parsing.c */
+int		ft_parse_input(t_shell *shell);
 
-/*expand.c*/
-void			ft_expand_tokens(t_shell *shell);
+//============================================================================//
+//                              TOKENIZATION                                  //
+//============================================================================//
+/* 510_tokenization/tokens.c */
+int		ft_tokenize(t_shell *shell);
 
-/*parsing.c*/
-int				ft_parse_input(t_shell *shell);
-
-/*syntax_validationc*/
-t_status		ft_validate_syntax(t_token *token);
-
-/*tokens.c*/
-int				ft_tokenize(t_shell *shell);
-
-/*tokens_utils.c*/
-void			ft_add_token_to_list(t_shell *shell, t_token *new_token);
-t_token			*ft_create_token(char *value, t_token_type type);
+//============================================================================//
+//                              TOKEN UTILITIES                               //
+//============================================================================//
+/* 510_tokenization/tokens_utils.c */
+void		ft_add_token_to_list(t_shell *shell, t_token *new_token);
+t_token		*ft_create_token(char *value, t_token_type type);
 t_token_type	ft_determine_token_type(char *value, size_t len);
 
-/*tokens_utils_2.c*/
-int				ft_create_and_add_token(t_shell *shell, size_t start,
-					size_t end, int quoted);
+/* 510_tokenization/tokens_utils_2.c */
+int		ft_create_and_add_token(t_shell *shell, size_t start, size_t end, int quoted);
 
-/* 600_exec */
-/* exec.c */
-void			ft_execute_input(t_shell *shell);
+//============================================================================//
+//                             SYNTAX VALIDATION                              //
+//============================================================================//
+/* 520_syntax_validation/syntax_validation.c */
+t_status	ft_validate_syntax(t_token *token);
 
-/* 610_builtins */
-/* echo.c */
-void			ft_echo(t_shell *shell);
-/* cd.c */
-void			ft_cd(t_shell *shell);
-/* env.c */
-void			ft_env(t_shell *shell);
+//============================================================================//
+//                             VARIABLE EXPANSION                             //
+//============================================================================//
+/* 530_expansion/expand.c */
+void		ft_expand_tokens(t_shell *shell);
 
-/* 700_clean*/
-/*clean.c*/
-void			ft_cleanup(t_shell *shell);
+//============================================================================//
+//                               REDIRECTIONS                                 //
+//============================================================================//
+/* 540_redirects/redirects.c */
+void		ft_create_redirection_list(t_shell *shell);
 
-/* 800_utils*/
-/* env_utils.c */
-char			**ft_duplicate_env(char **envp);
-char			*ft_get_env_value(char *var, char **env);
-int				ft_get_var_index(char *var, char **env);
-void			ft_set_env_value(char *var, char *value, char **env);
+//============================================================================//
+//                                EXECUTION                                   //
+//============================================================================//
+/* 600_exec/exec.c */
+void		ft_execute_input(t_shell *shell);
 
-/*check_type_functions.c*/
-int				ft_is_operator(char c);
-int				ft_is_space(char c);
-int				ft_is_quote(char c);
-int				ft_is_command(char *value, size_t len);
+//============================================================================//
+//                               BUILT-IN COMMANDS                            //
+//============================================================================//
+/* 610_builtins/echo.c */
+void		ft_echo(t_shell *shell);
 
-/*safe_functions.c */
-void			*ft_safe_malloc(size_t size);
-char			*ft_safe_readline(t_shell *shell);
+/* 610_builtins/cd.c */
+void		ft_cd(t_shell *shell);
+
+/* 610_builtins/env.c */
+void		ft_env(t_shell *shell);
+
+//============================================================================//
+//                                  CLEANUP                                   //
+//============================================================================//
+/* 700_clean/clean.c */
+void		ft_cleanup(t_shell *shell);
+
+//============================================================================//
+//                       ENVIRONMENT VARIABLE UTILITIES                       //
+//============================================================================//
+/* 800_utils/env_utils.c */
+char		**ft_duplicate_env(char **envp);
+char		*ft_get_env_value(char *var, char **env);
+int		ft_get_var_index(char *var, char **env);
+void		ft_set_env_value(char *var, char *value, char **env);
+
+//============================================================================//
+//                      CHARACTER AND COMMAND TYPE CHECKS                     //
+//============================================================================//
+/* 800_utils/check_type_functions.c */
+int		ft_is_operator(char c);
+int		ft_is_space(char c);
+int		ft_is_quote(char c);
+int		ft_is_command(char *value, size_t len);
+
+//============================================================================//
+//                          SAFE MEMORY AND IO OPERATIONS                     //
+//============================================================================//
+/* 800_utils/safe_functions.c */
+void		*ft_safe_malloc(size_t size);
+char		*ft_safe_readline(t_shell *shell);
 
 #endif
--- src/540_redirects/redirects.c
+++ src/540_redirects/redirects.c
@@ -6,7 +6,82 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 09:11:23 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/12 09:11:26 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/12 10:05:13 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../inc/minishell.h"
+
+static void	ft_create_and_add_redirect(t_token *token, t_shell *shell,
+				t_redirect **last_redirect);
+
+/**
+ * @brief Creates a linked list of redirection tokens from the shell's tokens.
+ *
+ * This function iterates through the shell's list of tokens and identifies
+ * tokens that represent redirection operators (e.g., REDIRECT_IN, REDIRECT_OUT,
+ * HEREDOC, REDIRECT_APPEND). For each redirection token found, it calls
+ * ft_create_and_add_redirect to add the redirection to the shell's redirection
+ * list.
+ *
+ * @param shell A pointer to the shell structure containing the token list from
+ *              which redirection tokens are to be extracted and added to the
+ *              redirection list.
+ */
+void	ft_create_redirection_list(t_shell *shell)
+{
+	t_token		*token;
+	t_redirect	*last_redirect;
+
+	token = shell->tokens;
+	last_redirect = NULL;
+	if (!token)
+		return ;
+	while (token)
+	{
+		if (token->type == REDIRECT_IN
+			|| token->type == REDIRECT_OUT
+			|| token->type == HEREDOC
+			|| token->type == REDIRECT_APPEND)
+		{
+			ft_create_and_add_redirect(token, shell, &last_redirect);
+			token = token->next->next;
+		}
+		token = token->next;
+	}
+}
+
+/**
+ * @brief Creates a new redirection token from a token and adds it to the
+ *        shell's redirection list.
+ *
+ * This function allocates memory for a new redirection token and copies the
+ * value of the given token into the new redirection token. It then finds the
+ * last redirection token in the shell's redirection list and appends the new
+ * redirection token to the list. If the shell's redirection list is initially
+ * empty, the new redirection token becomes the first token in the list.
+ *
+ * @param token A pointer to the token whose value is to be copied into the
+ *              new redirection token.
+ * @param shell A pointer to the shell structure containing the redirection
+ *              list to which the new redirection token is to be added.
+ */
+static void	ft_create_and_add_redirect(t_token *token, t_shell *shell,
+										t_redirect **last_redirect)
+{
+	t_redirect	*redirect;
+
+	redirect = ft_safe_malloc(sizeof(t_redirect));
+	if (!redirect)
+		return ;
+	redirect->type = token->type;
+	redirect->filename = ft_strdup(token->next->value);
+	if (!redirect->filename)
+		return ;
+	redirect->next = NULL;
+	if (!(*last_redirect))
+		shell->redirects = redirect;
+	else
+		(*last_redirect)->next = redirect;
+	(*last_redirect) = redirect;
+}
