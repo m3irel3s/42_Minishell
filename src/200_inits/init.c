bb8a1e708f6793e2d26d5c06d2ac3854d40011a5
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-12 09:06:31 +0000

    add redirections structure initialization

--- src/200_inits/init.c
+++ src/200_inits/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmeirele <jmeirele@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 13:59:06 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/11 13:31:46 by jmeirele         ###   ########.fr       */
+/*   Updated: 2025/02/12 09:06:17 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,5 @@ void	ft_init_shell(t_shell *shell, char **envp)
 	shell->prompt = NULL;
 	shell->input = NULL;
 	shell->tokens = NULL;
+	shell->redirects = NULL;
 }
