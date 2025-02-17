af647fd355914c3b5eb542769bd0ae23118122ae
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-17 14:07:38 +0000

    expansion working

--- src/510_tokenization/tokens_handlers_2.c
+++ src/510_tokenization/tokens_handlers_2.c
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 14:00:00 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/17 14:01:15 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/17 14:03:31 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,7 @@ t_status	ft_handle_word(t_shell *shell, size_t *i)
 	t_quote_info	quote_info;
 
 	start = *i;
-	quote_info.in_quotes = 0;
-	quote_info.quote_char = 0;
-	quote_info.quoted = 0;
+	ft_reset_quote_info(&quote_info);
 	if (ft_process_quoted_word(shell, i, &start, &quote_info) != SUCCESS)
 		return (ERROR);
 	if (*i > start)
@@ -44,7 +42,7 @@ static t_status	ft_process_quoted_word(t_shell *shell, size_t *i,
 	while (shell->input[*i] && ((!ft_is_space(shell->input[*i])
 				&& !ft_is_operator(shell->input[*i])) || quote_info->in_quotes))
 	{
-		if (ft_handle_quote(shell, i, start, &quote_info) != SUCCESS)
+		if (ft_handle_quote(shell, i, start, quote_info) != SUCCESS)
 			return (ERROR);
 		(*i)++;
 	}
