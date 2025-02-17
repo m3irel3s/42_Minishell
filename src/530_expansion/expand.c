705e43ddbf6eecb271a2011c9e57cbb8ec948708
Melanie Reis <melaniereis@Air-de-Melanie.Home>
2025-02-17 10:54:03 +0000

    start implementation of expansion

--- src/530_expansion/expand.c
+++ src/530_expansion/expand.c
@@ -6,14 +6,26 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:00:00 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/14 15:59:20 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/17 10:36:18 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-t_status ft_expand(t_shell *shell)
+t_status	ft_expand(t_shell *shell)
 {
-	(void)shell;
+	t_token *current;
+	char *expanded_value;
+
+	current = shell->tokens;
+	while (current)
+	{
+		expanded_value = ft_expand_token(shell, current->value);
+		if (!expanded_value)
+			return (ERROR);
+		free(current->value);
+		current->value = expanded_value;
+		current = current->next;
+	}
 	return (SUCCESS);
 }
