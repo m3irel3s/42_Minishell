caf78324578129b0c0cb93fd4e790e0f75fd78d9
Melanie Reis <melaniereis@Mac.Home>
2025-02-20 11:51:46 +0000

    correct norminette and divide function of redirection_utils into smaller ones

--- src/600_exec/exec_redirection_utils.c
+++ src/600_exec/exec_redirection_utils.c
@@ -6,29 +6,33 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 09:11:23 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/20 11:30:47 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/20 11:51:08 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-static void	ft_create_and_add_redirect(t_token *token, t_shell *shell,
-				t_redirect **last_redirect);
+static int		ft_is_redirect_token(int type);
+static t_token	*ft_update_tokens(t_shell *shell, t_token *token,
+					t_token *next_token, t_token *after_next);
+static void		ft_create_and_add_redirect(t_token *token, t_shell *shell,
+					t_redirect **last_redirect);
 
 /**
- * @brief Creates a linked list of redirection tokens from the shell's tokens.
+ * @brief Creates a list of redirections from the shell's token list.
  *
- * This function iterates through the shell's list of tokens and identifies
- * tokens that represent redirection operators (e.g., REDIRECT_IN, REDIRECT_OUT,
- * HEREDOC, REDIRECT_APPEND). For each redirection token found, it calls
- * ft_create_and_add_redirect to add the redirection to the shell's redirection
- * list.
+ * This function iterates through the shell's token list, identifies
+ * redirection tokens, and creates a linked list of redirection structures
+ * based on these tokens. For each identified redirection token, it checks
+ * if there is a following token representing the filename and creates a
+ * redirection structure using `ft_create_and_add_redirect`. It updates the
+ * token list to remove the redirection-related tokens using
+ * `ft_update_tokens`. If a redirection token is found without a following
+ * filename token, an error is printed and the function exits early.
  *
- * @param shell A pointer to the shell structure containing the token list from
- *              which redirection tokens are to be extracted and added to the
- *              redirection list.
+ * @param shell A pointer to the shell structure containing the token list.
  */
-void ft_create_redirection_list(t_shell *shell)
+void	ft_create_redirection_list(t_shell *shell)
 {
 	t_token		*token;
 	t_token		*next_token;
@@ -44,32 +48,65 @@ void ft_create_redirection_list(t_shell *shell)
 			after_next = NULL;
 		else
 			after_next = next_token->next;
-		if (token->type == REDIRECT_IN
-			|| token->type == REDIRECT_OUT
-			|| token->type == HEREDOC
-			|| token->type == REDIRECT_APPEND)
+		if (ft_is_redirect_token(token->type))
 		{
 			if (!next_token)
-			{
-				ft_print_redirect_no_file_error();
-				return ;
-			}
+				return (ft_print_redirect_no_file_error());
 			ft_create_and_add_redirect(token, shell, &last_redirect);
-			if (token->prev)
-				token->prev->next = after_next;
-			else
-				shell->tokens = after_next;
-			if (after_next)
-				after_next->prev = token->prev;
-			free(token->value);
-			free(token);
-			free(next_token->value);
-			free(next_token);
-			token = after_next;
+			token = ft_update_tokens(shell, token, next_token, after_next);
 		}
 		else
 			token = next_token;
 	}
+}
+
+/**
+ * @brief Determines if a token type corresponds to a redirection.
+ *
+ * This function checks if the provided token type is one of the redirection
+ * types, such as REDIRECT_IN, REDIRECT_OUT, REDIRECT_APPEND, or HEREDOC.
+ *
+ * @param type The token type to be checked.
+ * @return Returns 1 if the type is a redirection token, otherwise returns 0.
+ */
+static int	ft_is_redirect_token(int type)
+{
+	return (type == REDIRECT_IN
+		|| type == REDIRECT_OUT
+		|| type == HEREDOC
+		|| type == REDIRECT_APPEND);
+}
+
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * @brief Updates the token list and frees the given token and next token.
+ *
+ * This function updates the token list by removing the given token and its
+ * next token, and links the previous token to the token after the next token
+ * (or sets the shell's tokens to NULL if the token is the first token in the
+ * list). It also frees the given token and its next token.
+ *
+
+ * @param shell A pointer to the shell structure.
+ * @param token The token to be removed.
+ * @param next_token The token after the given token.
+ * @param after_next The token after the next token.
+ * @return The updated token list.
+ */
+static t_token	*ft_update_tokens(t_shell *shell, t_token *token,
+	t_token *next_token, t_token *after_next)
+{
+	if (token->prev)
+		token->prev->next = after_next;
+	else
+		shell->tokens = after_next;
+	if (after_next)
+		after_next->prev = token->prev;
+	free(token->value);
+	free(token);
+	free(next_token->value);
+	free(next_token);
+	return (after_next);
 }
 
 /**
@@ -88,7 +125,6 @@ void ft_create_redirection_list(t_shell *shell)
  * @param last_redirect A double pointer to the last redirection in the list,
  *                      used to append the newly created redirection.
  */
-
 static void	ft_create_and_add_redirect(t_token *token, t_shell *shell,
 										t_redirect **last_redirect)
 {
