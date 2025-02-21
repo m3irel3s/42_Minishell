6acf859344d04eb52b9d246f81721d907325ddb2
Melanie Reis <melaniereis@Mac.Home>
2025-02-21 08:48:24 +0000

    change expand files to update exit statud and improve error handling, minor improvments also of the code itself made

--- src/530_expansion/expand.c
+++ src/530_expansion/expand.c
@@ -6,38 +6,50 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:00:00 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/20 08:19:46 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/21 08:46:30 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 static char	*ft_expand_token(t_shell *shell, char *token, int quoted);
+static char	*ft_handle_expansion_error(t_shell *shell);
+static char	*ft_join_and_free(char *expanded_value, char *temp);
 
 /**
- * @brief Expand tokens in the token list, replacing '$' with their expanded
- *        values.
+ * @brief Expands all tokens in the shell by replacing any '$' characters with
+ *        their environment values.
  *
- * This function iterates through the token list, expanding each token by calling
- * ft_expand_token. If any expansion fails (i.e. returns NULL), the function
- * immediately returns ERROR. Otherwise, it replaces the original token value
- * with the expanded value and moves on to the next token.
+ * This function iterates through all tokens in the shell and expands any tokens
+ * that contain a '$' character. The expanded string is then placed in the
+ * token's value field.
  *
- * @param shell The shell struct containing the token list and environment info.
- * @return SUCCESS if all tokens expanded successfully, ERROR otherwise.
+ * @param shell The shell struct containing the tokens to be expanded.
+ *
+ * @return SUCCESS if all tokens are successfully expanded, otherwise ERROR.
  */
 t_status	ft_expand(t_shell *shell)
 {
 	t_token	*current;
 	char	*expanded_value;
 
+	if (!shell || !shell->tokens)
+	{
+		ft_putstr_fd(ERR_INVALID_PARAMS, STDERR_FILENO);
+		if (shell)
+			shell->exit_status = EXIT_FAILURE;
+		return (ERROR);
+	}
 	current = shell->tokens;
 	while (current)
 	{
 		expanded_value = ft_expand_token(shell, current->value,
 				current->quoted);
 		if (!expanded_value)
+		{
+			shell->exit_status = EXIT_FAILURE;
 			return (ERROR);
+		}
 		ft_free(current->value);
 		current->value = expanded_value;
 		current = current->next;
@@ -46,17 +58,19 @@ t_status	ft_expand(t_shell *shell)
 }
 
 /**
- * @brief Expands a single token, replacing '$' characters with their expanded
- *        values according to the shell's environment.
+ * @brief Expands a token string to replace any '$' characters with their
+ *        environment values.
  *
- * This function iterates through the token string, replacing any '$' characters
- * with their expanded values using ft_handle_dollar. If the token is quoted,
- * '$' characters are treated as literal characters.
+ * This function iterates through the input token string, replacing any '$'
+ * characters with their environment values using ft_handle_dollar. The
+ * expanded string is then returned. If memory allocation fails, the function
+ * returns NULL and the shell's exit status is set to EXIT_FAILURE.
  *
- * @param shell The shell struct containing the environment info.
- * @param token The string token to be expanded.
- * @param quoted The quoted status of the token (1 if quoted, 0 otherwise).
- * @return The expanded token value as a string, or NULL if an error occurred.
+ * @param shell The shell structure containing environment and state info.
+ * @param token The input token string to be expanded.
+ * @param quoted Whether the token is quoted (1 if quoted, 0 otherwise).
+ *
+ * @return A string representing the expanded value, or NULL on failure.
  */
 static char	*ft_expand_token(t_shell *shell, char *token, int quoted)
 {
@@ -64,7 +78,11 @@ static char	*ft_expand_token(t_shell *shell, char *token, int quoted)
 	char	*temp;
 	size_t	i;
 
-	expanded_value = ft_strdup("");
+	if (!token)
+		return (NULL);
+	expanded_value = ft_strdup_safe("");
+	if (!expanded_value)
+		return (ft_handle_expansion_error(shell));
 	i = 0;
 	while (token[i])
 	{
@@ -72,30 +90,65 @@ static char	*ft_expand_token(t_shell *shell, char *token, int quoted)
 		{
 			temp = ft_handle_dollar(shell, token, &i);
 			if (temp)
-			{
-				expanded_value = ft_strjoin_gnl(expanded_value, temp);
-				ft_free(temp);
-			}
+				expanded_value = ft_join_and_free(expanded_value, temp);
 		}
 		else
-		{
-			expanded_value = ft_process_char(expanded_value, token[i]);
-			i++;
-		}
+			expanded_value = ft_process_char(expanded_value, token[i++]);
+		if (!expanded_value)
+			return (ft_handle_expansion_error(shell));
 	}
 	return (expanded_value);
 }
 
 /**
+ * @brief Concatenates two strings using ft_strjoin_gnl and frees them.
+ *
+ * This function is used to concatenate the expanded value with the temp string
+ * and then free both the expanded value and temp strings.
+ *
+ * @param expanded_value Expanded value string.
+ * @param temp Temporary string.
+ * @return The concatenated string.
+ */
+static char	*ft_join_and_free(char *expanded_value, char *temp)
+{
+	char	*result;
+
+	result = ft_strjoin_gnl(expanded_value, temp);
+	ft_free(temp);
+	return (result);
+}
+
+/**
+ * @brief Handles an error during token expansion.
+ *
+ * This function sets the shell's exit status to EXIT_FAILURE to indicate
+ * that an error occurred during the expansion process. It returns NULL
+ * to signal that the expansion was unsuccessful.
+ *
+ * @param shell A pointer to the shell structure to update the exit status.
+ * @return Always returns NULL to indicate an expansion error.
+ */
+
+static char	*ft_handle_expansion_error(t_shell *shell)
+{
+	shell->exit_status = EXIT_FAILURE;
+	return (NULL);
+}
+
+/**
  * @brief Appends a character to the expanded value string.
  *
- * This function takes an expanded value string and a character, converts
- * the character to a string, appends it to the expanded value, and returns
- * the new expanded value. The original expanded value is freed.
+ * This function takes a character and appends it to the given expanded
+ * value string. The function allocates a new string with the appended
+ * character, frees the original expanded value string, and returns the
+ * new expanded string.
  *
- * @param expanded_value The current expanded value string.
- * @param c The character to be appended to the expanded value.
- * @return The new expanded value string with the character appended.
+ * @param expanded_value The current expanded string to which the character
+ * will be appended.
+ * @param c The character to append to the expanded value string.
+ * @return A new string with the character appended, or NULL if memory
+ * allocation fails.
  */
 
 char	*ft_process_char(char *expanded_value, char c)
@@ -105,7 +158,6 @@ char	*ft_process_char(char *expanded_value, char c)
 
 	c_str[0] = c;
 	c_str[1] = '\0';
-	new_expanded = ft_strjoin(expanded_value, c_str);
-	ft_free(expanded_value);
+	new_expanded = ft_strjoin_gnl(expanded_value, c_str);
 	return (new_expanded);
 }
