10f66dd89c3221021f643cc0b589368739b705a9
Melanie Reis <melaniereis@Mac.Home>
2025-02-21 08:23:35 +0000

    add comments to syntax_validation

--- src/520_errors_handler/syntax_validation.c
+++ src/520_errors_handler/syntax_validation.c
@@ -6,14 +6,29 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 21:43:44 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/21 08:17:37 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/21 08:23:19 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-static t_status	ft_validate_pipe(t_shell *shell, t_token *current, t_token *prev);
+static t_status	ft_validate_pipe(t_shell *shell, t_token *current,
+	t_token *prev);
 static t_status	ft_validate_redirect(t_shell *shell, t_token *current);
+
+/**
+ * @brief Validates the syntax of a linked list of tokens.
+ *
+ * Checks that the linked list of tokens is syntactically valid. If the list
+ * is invalid, prints an error message and updates the shell's exit status.
+ * If valid, returns SUCCESS.
+ *
+ * @param shell The shell structure containing the linked list of tokens to
+ * validate.
+ * @param tokens The linked list of tokens to validate.
+ * @return Returns SUCCESS if the linked list of tokens is syntactically valid;
+ * otherwise, returns ERROR.
+ */
 
 t_status	ft_validate_syntax(t_shell *shell, t_token *tokens)
 {
@@ -47,6 +62,19 @@ t_status	ft_validate_syntax(t_shell *shell, t_token *tokens)
 	return (SUCCESS);
 }
 
+/**
+ * @brief Validates the syntax of a pipe token.
+ *
+ * Checks if the previous token is not a pipe and if there is a next token.
+ * If not, prints an error message and sets the shell's exit status to
+ * EXIT_FAILURE. Otherwise, returns SUCCESS.
+ *
+ * @param shell The shell structure containing the tokens to validate.
+ * @param current The pipe token to validate.
+ * @param prev The previous token of the pipe token.
+ *
+ * @return Returns SUCCESS if the pipe token is valid; otherwise, returns ERROR.
+ */
 static t_status	ft_validate_pipe(t_shell *shell, t_token *current,
 		t_token *prev)
 {
@@ -54,6 +82,22 @@ static t_status	ft_validate_pipe(t_shell *shell, t_token *current,
 		return (ft_print_syntax_error(shell, "|"));
 	return (SUCCESS);
 }
+
+/**
+ * @brief Validates the syntax of a redirection token.
+ *
+ * This function checks if the redirection token is followed by a valid
+ * token. It ensures that a filename follows redirection tokens, and
+ * that a valid delimiter follows heredoc tokens. If these conditions
+ * are not met, the appropriate error message is printed.
+ *
+ * @param shell A pointer to the shell structure used for error reporting.
+ * @param current A pointer to the current token being validated.
+ *
+ * @return Returns SUCCESS if the redirection token is valid; otherwise,
+ * returns ERROR if there's no following valid token or if there is a
+ * heredoc token with an empty delimiter.
+ */
 
 static t_status	ft_validate_redirect(t_shell *shell, t_token *current)
 {
