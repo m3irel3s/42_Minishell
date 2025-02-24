cc01ffcd3ebe541814f7f5f84fbaa23cde063ad2
Melanie Reis <melaniereis@MacBook-Air-de-Melanie.local>
2025-02-24 09:18:31 +0000

    update env to update exit_Status

--- src/610_builtins/env.c
+++ src/610_builtins/env.c
@@ -6,25 +6,24 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 14:15:19 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/02/21 10:57:58 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/24 09:17:56 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+#include <unistd.h>
 
 void	ft_env(t_shell *shell)
 {
 	char	**env;
-	char	*res;
 	int		i;
 
 	i = 0;
 	env = shell->env_cpy;
 	while (env[i])
 	{
-		res = env[i];
-		ft_printf(STDOUT_FILENO, "%s\n", res);
+		ft_printf(STDOUT_FILENO, "%s\n", env[i]);
 		i++;
 	}
-	return ;
+	g_exit_status = EXIT_SUCCESS;
 }
