e84ebe764a603f70d83c491c26666a6b9bc31b08
Melanie Reis <melaniereis@MacBook-Air-de-Melanie.local>
2025-02-24 09:37:49 +0000

    update pwd to handle exit_Status

--- src/610_builtins/pwd.c
+++ src/610_builtins/pwd.c
@@ -6,17 +6,25 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 13:22:08 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/02/21 17:11:17 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/24 09:37:26 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+#include <unistd.h>
 
 void	ft_pwd(void)
 {
 	char	*path;
 
 	path = ft_get_current_directory();
-	ft_printf(1, "%s\n", path);
+	if (!path)
+	{
+		ft_print_error(ERR_GET_CWD_FAIL);
+		g_exit_status = EXIT_FAILURE;
+		return ;
+	}
+	ft_printf(STDOUT_FILENO, "%s\n", path);
 	ft_free(path);
+	g_exit_status = EXIT_SUCCESS;
 }
