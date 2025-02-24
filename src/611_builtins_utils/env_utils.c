76b607932695d8a957e86b39be806fa005e16259
Melanie Reis <melaniereis@MacBook-Air-de-Melanie.local>
2025-02-24 09:50:33 +0000

    improve env utils

--- src/611_builtins_utils/env_utils.c
+++ src/611_builtins_utils/env_utils.c
@@ -6,12 +6,18 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:50:07 by jmeirele          #+#    #+#             */
-/*   Updated: 2025/02/21 11:06:07 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/24 09:50:17 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+/**
+ * @brief Duplicates the environment array.
+ *
+ * @param envp The original environment array to duplicate.
+ * @return A newly allocated copy of the environment array, or NULL on failure.
+ */
 char	**ft_duplicate_env(char **envp)
 {
 	char	**env_cpy;
@@ -20,29 +26,41 @@ char	**ft_duplicate_env(char **envp)
 	if (!envp)
 		return (NULL);
 	i = 0;
-	env_cpy = NULL;
 	while (envp[i])
 		i++;
 	env_cpy = ft_safe_malloc(sizeof(char *) * (i + 1));
+	if (!env_cpy)
+		return (NULL);
 	i = 0;
 	while (envp[i])
 	{
 		env_cpy[i] = ft_safe_strdup(envp[i]);
+		if (!env_cpy[i])
+		{
+			ft_free_arr(env_cpy);
+			return (NULL);
+		}
 		i++;
 	}
 	env_cpy[i] = NULL;
 	return (env_cpy);
 }
 
+/**
+ * @brief Gets the size of the environment array.
+ *
+ * @param shell The shell structure containing the environment.
+ * @return The number of environment variables, or -1 if the environment is NULL.
+ */
 int	ft_get_env_size(t_shell *shell)
 {
 	int		i;
 	char	**env;
 
 	env = shell->env_cpy;
-	i = 0;
 	if (!env)
 		return (-1);
+	i = 0;
 	while (env[i])
 		i++;
 	return (i);
