b38cee67b4d72a1262218bb0a364a626a32d1c37
Melanie Reis <melaniereis@MacBook-Air-de-Melanie.local>
2025-02-25 10:11:35 +0000

    restart parsing, rewrite everything

--- src/500_parsing/parsing.c
+++ src/500_parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 13:29:46 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/21 16:56:14 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/24 21:37:15 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,45 @@
 static t_status	ft_validate_all_tokens(t_shell *shell);
 
 /**
- * @brief Parses the input string of the shell, tokenizing it, validating its
- * syntax, expanding variables, and setting up redirections.
+ * @brief Parses the input string in the shell structure.
+ *
+ * This function expands the input string, splits it into tokens, creates token
+ * structures, and validates the tokens.
  *
  * @param shell A pointer to the shell structure containing the input string.
- *
- * @return Returns SUCCESS if the input is successfully parsed; otherwise,
- * returns ERROR.
+ * @return SUCCESS if the input is parsed successfully, or an error status if
+ * there is a failure during any step.
  */
 t_status	ft_parse_input(t_shell *shell)
 {
-	if (ft_tokenize(shell) != SUCCESS)
+	char	**split_input;
+
+	if (!shell || !shell->input)
 		return (ft_print_error(ERR_TOKENIZATION_FAIL));
-
-	if (ft_validate_all_tokens(shell) != SUCCESS)
-		return (ft_print_error(ERR_SYNTAX_VALIDATION_FAIL));
-
 	if (ft_expand(shell) != SUCCESS)
 		return (ft_print_error(ERR_EXPANSION_FAIL));
-
+	split_input = ft_split_input(shell->input);
+	if (!split_input)
+		return (ft_print_error(ERR_TOKENIZATION_FAIL));
+	if (ft_tokenize(shell, split_input) != SUCCESS)
+	{
+		ft_free_arr(split_input);
+		return (ft_print_error(ERR_TOKENIZATION_FAIL));
+	}
+	ft_free_arr(split_input);
+	if (ft_validate_all_tokens(shell) != SUCCESS)
+	{
+		ft_free_tokens(&shell->tokens);
+		return (ft_print_error(ERR_SYNTAX_VALIDATION_FAIL));
+	}
 	g_exit_status = EXIT_SUCCESS;
 	return (SUCCESS);
 }
 
 /**
  * @brief Validates the syntax of all tokens in the shell structure.
+ *
+ * Iterates through the token list and validates the syntax of each token.
  *
  * @param shell A pointer to the shell structure containing the tokens.
  *
