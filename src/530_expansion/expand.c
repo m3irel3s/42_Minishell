2608e14ca02bd9365c2b4d0f8b1462144e5cd732
Melanie Reis <melaniereis@MacBook-Air-de-Melanie.local>
2025-02-25 10:16:08 +0000

    correct expansion

--- src/530_expansion/expand.c
+++ src/530_expansion/expand.c
@@ -6,120 +6,16 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:00:00 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/21 17:22:39 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/24 17:35:10 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-static char	*ft_expand_token(t_shell *shell, char *token, int quoted);
-static char	*ft_handle_expansion_error(void);
+//static char	*ft_handle_expansion_error(void);
 static char	*ft_join_and_free(char *expanded_value, char *temp);
-static char	*ft_handle_null_expansion(char *expanded_value);
+//static char	*ft_handle_null_expansion(char *expanded_value);
 
-/**
- * @brief Expands all tokens in the shell's linked list.
- *
- * Iterates through the shell's linked list of tokens and expands each token by
- * calling ft_expand_token. The expanded token value is then stored in the
- * token's value field, and the original token value is freed. If memory
- * allocation fails during expansion, the function sets the shell's exit status
- * to failure and returns an error status.
- *
- * @param shell A pointer to the shell structure containing the linked list of
- * tokens to be expanded.
- * @return SUCCESS if all tokens are expanded successfully, or ERROR if there
- * is a memory allocation failure.
- */
-t_status	ft_expand(t_shell *shell)
-{
-	t_token	*current;
-	char	*expanded_value;
-
-	if (!shell)
-		return (ft_print_error(ERR_INVALID_SHELL));
-	if (!shell->tokens)
-		return (SUCCESS);
-	current = shell->tokens;
-	while (current)
-	{
-		expanded_value = ft_expand_token(shell, current->value,
-				current->quoted);
-		if (!expanded_value)
-		{
-			g_exit_status = EXIT_FAILURE;
-			return (ERROR);
-		}
-		ft_free(current->value);
-		current->value = expanded_value;
-		current = current->next;
-	}
-	return (SUCCESS);
-}
-
-/**
- * @brief Expands a single token, replacing any '$' characters with their values.
- *
- * This function iterates through the token, replacing any '$' characters with
- * their expanded values using ft_handle_dollar. The expanded value is then
- * returned.
- * If memory allocation fails, the function sets the shell's exit status to
- * failure and returns NULL.
- *
- * @param shell A pointer to the shell structure containing environment info.
- * @param token The token to be expanded.
- * @param quoted A boolean indicating if the token is quoted.
- * @return The expanded token value as a string, or NULL on failure.
- */
-static char	*ft_expand_token(t_shell *shell, char *token, int quoted)
-{
-	char	*expanded_value;
-	char	*temp;
-	size_t	i;
-
-	if (!token)
-		return (ft_safe_strdup(""));
-	expanded_value = ft_safe_strdup("");
-	if (!expanded_value)
-		return (ft_handle_expansion_error());
-	i = 0;
-	while (token[i])
-	{
-		if (token[i] == '$' && !quoted)
-		{
-			temp = ft_handle_dollar(shell, token, &i);
-			if (temp)
-				expanded_value = ft_join_and_free(expanded_value, temp);
-			else
-				expanded_value = ft_handle_null_expansion(expanded_value);
-		}
-		else
-			expanded_value = ft_process_char(expanded_value, token[i++]);
-		if (!expanded_value)
-			return (ft_handle_expansion_error());
-	}
-	return (expanded_value);
-}
-
-/**
- * @brief Joins two strings, freeing the second string after the join.
- *
- * This function is used to join a temporary string to the expanded value of a
- * token, freeing the temporary string after the join.
- *
- * @param shell A pointer to the shell structure containing environment info.
- * @param expanded_value The current expanded value of a token.
- * @param temp The temporary string to be joined.
- * @return The joined string.
- */
-static char	*ft_join_and_free(char *expanded_value, char *temp)
-{
-	char	*result;
-
-	result = ft_safe_strjoin(expanded_value, temp, 1);
-	ft_free(temp);
-	return (result);
-}
 
 /**
  * @brief Handles an expansion error, setting the shell's exit status to failure.
@@ -130,11 +26,11 @@ static char	*ft_join_and_free(char *expanded_value, char *temp)
  * @param shell A pointer to the shell structure containing environment info.
  * @return NULL to indicate that an error occurred.
  */
-static char	*ft_handle_expansion_error(void)
-{
-	g_exit_status = EXIT_FAILURE;
-	return (NULL);
-}
+// static char	*ft_handle_expansion_error(void)
+// {
+// 	g_exit_status = EXIT_FAILURE;
+// 	return (NULL);
+// }
 
 /**
  * @brief Processes a single character in a token during expansion.
@@ -162,6 +58,92 @@ char	*ft_process_char(char *expanded_value, char c)
 }
 
 /**
+ * @brief Joins two strings, freeing the second string after the join.
+ *
+ * This function is used to join a temporary string to the expanded value of a
+ * token, freeing the temporary string after the join.
+ *
+ * @param shell A pointer to the shell structure containing environment info.
+ * @param expanded_value The current expanded value of a token.
+ * @param temp The temporary string to be joined.
+ * @return The joined string.
+ */
+static char	*ft_join_and_free(char *expanded_value, char *temp)
+{
+	char	*result;
+
+	result = ft_safe_strjoin(expanded_value, temp, 1);
+	ft_free(temp);
+	return (result);
+}
+
+/**
+ * @brief Handles quotes in the input string.
+ *
+ * This function updates the quote state based on the current character
+ * in the input string.
+ *
+ * @param input The input string.
+ * @param i A pointer to the current index in the input string.
+ * @param quote_state A pointer to the current quote state.
+ */
+static void ft_handle_quotes(char *input, size_t *i, int *quote_state)
+{
+	if (input[*i] == '\'' && *quote_state != 2)
+	{
+		if (*quote_state == 1)
+			*quote_state = 0;
+		else
+			*quote_state = 1;
+	}
+	else if (input[*i] == '\"' && *quote_state != 1)
+	{
+		if (*quote_state == 2)
+			*quote_state = 0;
+		else
+			*quote_state = 2;
+	}
+}
+
+/**
+ * @brief Expands a token in the input string.
+ *
+ * This function handles the expansion of a token in the input string,
+ * including handling quotes and dollar signs.
+ *
+ * @param shell A pointer to the shell structure containing the input string.
+ * @param input The input string to be expanded.
+ * @param i A pointer to the current index in the input string.
+ * @param quote_state A pointer to the current quote state.
+ * @return The expanded token string.
+ */
+static char *ft_expand_token(t_shell *shell, char *input, size_t *i, int *quote_state)
+{
+	char *expanded_value;
+	char *temp;
+
+	expanded_value = ft_safe_strdup("");
+	if (!expanded_value)
+		return (NULL);
+
+	while (input[*i])
+	{
+		ft_handle_quotes(input, i, quote_state);
+		if (input[*i] == '$' && *quote_state != 1)
+		{
+			temp = ft_handle_dollar(shell, input, i);
+			expanded_value = ft_join_and_free(expanded_value, temp);
+		}
+		else
+			expanded_value = ft_process_char(expanded_value, input[*i]);
+		(*i)++;
+		if (!expanded_value)
+			return (NULL);
+	}
+	return (expanded_value);
+}
+
+/**
  * @brief Handles the scenario where a variable expansion results in NULL.
  *
  * This function safely joins the current expanded value with an empty string,
@@ -175,12 +157,49 @@ char	*ft_process_char(char *expanded_value, char c)
  * @return The updated expanded value with an empty string appended, or NULL
  * if a memory allocation error occurs.
  */
-static char	*ft_handle_null_expansion(char *expanded_value)
-{
-	char	*result;
+// static char	*ft_handle_null_expansion(char *expanded_value)
+// {
+// 	char	*result;
 
-	result = ft_safe_strjoin(expanded_value, "", 1);
-	if (!result)
-		return (ft_handle_expansion_error());
-	return (result);
+// 	result = ft_safe_strjoin(expanded_value, "", 1);
+// 	if (!result)
+// 		return (ft_handle_expansion_error());
+// 	return (result);
+// }
+
+/**
+ * @brief Expands the input string in the shell structure.
+ *
+ * This function expands the input string by replacing any '$' characters with
+ * their expanded values. It also handles quoted strings and ensures that quotes
+ * are properly matched.
+ *
+ * @param shell A pointer to the shell structure containing the input string.
+ * @return SUCCESS if the input is expanded successfully, or an error status if
+ * there is a memory allocation failure or unmatched quotes.
+ */
+t_status	ft_expand(t_shell *shell)
+{
+	char *expanded_value;
+	size_t i;
+	int quote_state;
+
+	if (!shell || !shell->input)
+		return (ft_print_error(ERR_INVALID_SHELL_OR_INPUT));
+	expanded_value = ft_safe_strdup("");
+	if (!expanded_value)
+		return (ft_print_error(ERR_MALLOC_FAIL));
+	i = 0;
+	quote_state = 0;
+	while (shell->input[i])
+	{
+		expanded_value = ft_expand_token(shell, shell->input, &i, &quote_state);
+		if (!expanded_value)
+			return (ft_print_error(ERR_EXPANSION_FAIL));
+	}
+	if (quote_state != 0)
+		return (ft_print_error(ERR_SYNTAX_UNCLOSED_QUOTE));
+	free(shell->input);
+	shell->input = expanded_value;
+	return (SUCCESS);
 }
