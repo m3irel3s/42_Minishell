08f35cd90b7cede2c23cb429f9dffdcfbd0fcf91
Melanie Reis <melaniereis@MacBook-Air-de-Melanie.local>
2025-02-26 14:48:53 +0000

    update tokenization

--- src/510_tokenization/tokens_count.c
+++ src/510_tokenization/tokens_count.c
@@ -6,21 +6,19 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 20:04:16 by meferraz          #+#    #+#             */
-/*   Updated: 2025/02/26 10:25:57 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/02/26 11:40:11 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
-#include <stdio.h> // For debug statements
 
-static void	ft_count_word(char *input, size_t *i, int *in_quotes);
+static void	ft_skip_spaces(char *input, size_t *i);
+static void	ft_handle_operator(char *input, size_t *i, int *count);
+static void	ft_handle_quotes(char *input, size_t *i, int *in_quotes, char *q_char);
+static void	ft_count_word(char *input, size_t *i, int *in_quotes, char *q_char);
 
 /**
- * @brief Counts the number of words in the input string.
- *
- * This function counts the number of words in the input string, respecting
- * quotes and treating operators as separate tokens.
- *
+ * @brief Counts the number of words (tokens) in the input string.
  * @param input The input string to be counted.
  * @return The number of words in the input string, or -1 if input is NULL.
  */
@@ -29,79 +27,90 @@ int	ft_count_words(char *input)
 	size_t	i;
 	int		count;
 	int		in_quotes;
+	char	q_char;
 
 	if (!input)
 		return (-1);
 	i = 0;
 	count = 0;
 	in_quotes = 0;
-	printf("[DEBUG] Starting ft_count_words with input: '%s'\n", input);
+	q_char = 0;
 	while (input[i])
 	{
-		printf("[DEBUG] Processing character '%c' at index %zu\n", input[i], i);
-		while (input[i] && ft_is_space(input[i]))
-		{
-			printf("[DEBUG] Skipping space at index %zu\n", i);
-			i++;
-		}
+		ft_skip_spaces(input, &i);
 		if (!input[i])
 			break ;
 		if (ft_is_operator(input[i]))
-		{
-			count++;
-			printf("[DEBUG] Found operator '%c' at index %zu (count=%d)\n", input[i], i, count);
-			if (ft_is_double_operator(&input[i]))
-			{
-				printf("[DEBUG] Double operator found at index %zu\n", i);
-				i += 2;
-			}
-			else
-				i++;
-		}
+			ft_handle_operator(input, &i, &count);
 		else
 		{
 			count++;
-			printf("[DEBUG] Found word start at index %zu (count=%d)\n", i, count);
-			while (input[i] && (in_quotes || (!ft_is_space(input[i]) && !ft_is_operator(input[i]))))
-			{
-				ft_count_word(input, &i, &in_quotes);
-				printf("[DEBUG] Word processing at index %zu, in_quotes=%d\n", i, in_quotes);
-			}
+			ft_count_word(input, &i, &in_quotes, &q_char);
 		}
 	}
-	printf("[DEBUG] Finished ft_count_words. Total words counted: %d\n", count);
 	return (count);
 }
 
 /**
- * @brief Counts characters in a word, handling quotes.
- *
- * This function advances the index `i` while considering quoted sections in the
- * input string. It updates the `in_quotes` flag to track if the current position
- * is within quotes, ensuring that quoted substrings are handled correctly.
- *
- * @param input The input string being processed.
+ * @brief Skips spaces in the input string.
+ * @param input The input string.
+ * @param i Pointer to the current index in the input string.
+ */
+static void	ft_skip_spaces(char *input, size_t *i)
+{
+	while (input[*i] && ft_is_space(input[*i]))
+		(*i)++;
+}
+
+/**
+ * @brief Handles operators in the input string.
+ * @param input The input string.
+ * @param i Pointer to the current index in the input string.
+ * @param count Pointer to the token count.
+ */
+static void	ft_handle_operator(char *input, size_t *i, int *count)
+{
+	(*count)++;
+	if (ft_is_double_operator(&input[*i]))
+		*i += 2;
+	else
+		(*i)++;
+}
+
+/**
+ * @brief Handles quotes in the input string.
+ * @param input The input string.
  * @param i Pointer to the current index in the input string.
  * @param in_quotes Pointer to the flag indicating if currently in quotes.
+ * @param q_char Pointer to the current quote character.
  */
-static void	ft_count_word(char *input, size_t *i, int *in_quotes)
+static void	ft_handle_quotes(char *input, size_t *i, int *in_quotes, char *q_char)
 {
-	char	quote_char;
-
-	quote_char = 0;
-	printf("[DEBUG] ft_count_word processing character '%c' at index %zu\n", input[*i], *i);
-	if ((input[*i] == '\'' || input[*i] == '"') && !*in_quotes)
+	if ((input[*i] == '\'' || input[*i] == '"') && (!*in_quotes || input[*i] == *q_char))
 	{
-		*in_quotes = 1;
-		quote_char = input[*i];
-		printf("[DEBUG] Entering quotes with '%c' at index %zu\n", quote_char, *i);
-	}
-	else if (*in_quotes && input[*i] == quote_char)
-	{
-		*in_quotes = 0;
-		quote_char = 0;
-		printf("[DEBUG] Exiting quotes at index %zu\n", *i);
+		if (*in_quotes)
+		{
+			*in_quotes = 0;
+			*q_char = 0;
+		}
+		else
+		{
+			*in_quotes = 1;
+			*q_char = input[*i];
+		}
 	}
 	(*i)++;
-	printf("[DEBUG] Moving to next character at index %zu\n", *i);
+}
+
+/**
+ * @brief Counts a single word in the input string.
+ * @param input The input string.
+ * @param i Pointer to the current index in the input string.
+ * @param in_quotes Pointer to the flag indicating if currently in quotes.
+ * @param q_char Pointer to the current quote character.
+ */
+static void	ft_count_word(char *input, size_t *i, int *in_quotes, char *q_char)
+{
+	while (input[*i] && (*in_quotes || (!ft_is_space(input[*i]) && !ft_is_operator(input[*i]))))
+		ft_handle_quotes(input, i, in_quotes, q_char);
 }
